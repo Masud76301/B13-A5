b113910ed3afc36579880abbbeaae9e5ce148d78
Masud76301 <khaledmasud76301@gmail.com>
2026-03-10 23:13:04 +0600

    Modal functionality done

--- issue.js
+++ issue.js
@@ -1,12 +1,14 @@
 const issueCards = document.getElementById('issue-cards');
 const loadingSpinner = document.getElementById('load-spinner');
 const issuesCount = document.getElementById('issues-count');
+const issueModal = document.getElementById('issue-modal');
 
 showLoading = () => {
     loadingSpinner.classList.remove("hidden");
     loadingSpinner.classList.add("flex");
     issueCards.innerHTML = "";
 }
+
 hideLoading = () => {
     loadingSpinner.classList.add("hidden");
 }
@@ -28,15 +30,70 @@ selectBtn = (id) => {
     }
     else if (id == "openBtn") {
         openIssues();
-     
+
     }
     else {
         closedIssues();
- 
+
     }
 }
 
+showIssueModal = async (id) => {
+   
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res = await fetch(url);
+    const issue = await res.json();
+    const modal = document.createElement('div');
+    modal.className="modal-box";
+    modal.innerHTML = `
+               
+                    <h3 class="text-lg font-bold">${issue.data.title}</h3>
+                    <div class="flex gap-2 items-center">
+                         <h1 class="${issue.data.status == "open" ? "bg-green-600" : "bg-red-600"} text-white text-[12px] px-7 py-[2px] text-center rounded-3xl">${issue.data.status}</h1>
+                         <ul class="flex space-x-1  items-center list-inside list-disc">
+                            <li class="text-[12px] text-[#64748B]">Opened by ${issue.data.assignee}</li>
+                            <li class="text-[12px] text-[#64748B]">${new Date(issue.data.updatedAt).toLocaleDateString("en-GB")}</li>
+                         </ul>
+                         
+                    </div>
+                    <br>
+                        <!-- labels -->
+                        <div class="pb-5 mt-4 ">
+                            <span class="bg-error/10 text-red-500 border border-red-200 h-[18px] text-[10px] font-medium py-[2px] px-[4px] text-center rounded-3xl">${issue.data.labels[0].toUpperCase()}</span>
 
+                            ${issue.data.labels[1] != undefined ? `
+                                <span class="bg-warning/10 text-yellow-600 border border-yellow-300 h-[18px] text-[10px] font-medium py-[2px] px-[4px] text-center rounded-3xl">
+                                ${issue.data.labels[1].toUpperCase()}
+                                </span>`: ""}
+                        </div>
+                    
+                    <br>
+                    <p class="text-[12px] font-normal text-[#64748B]">${issue.data.description}</p>
+                    <br>
+                    <div class="flex gap-2 p-2 bg-base-200 rounded-md">
+                        <div class="w-[50%]">
+                            <p class="text-[11px] font-normal text-[#64748B]">Assignee:</p>
+                            <p class="text-[11px] font-bold">${issue.data.assignee}</p>
+                        </div>
+
+                        <div class="w-[50%]">
+                            <p class="text-[11px] font-normal text-[#64748B]">Priority:</p>
+                           <h1 class="${{medium: "bg-yellow-500",low: "bg-slate-300",high: "bg-red-600"}[issue.data.priority]} text-white text-[10px] w-[60px] py-[1px] text-center rounded-3xl">${issue.data.priority}</h1>
+                        </div>
+                    </div>
+                    <div class="modal-action">
+                        <form method="dialog">
+                            <!-- if there is a button in form, it will close the modal -->
+                            <button class="btn btn-primary">Close</button>
+                        </form>
+                    </div>
+                
+    `
+    
+    issueModal.appendChild(modal);
+    issueModal.showModal();
+    
+}
 
 
 
@@ -83,12 +140,15 @@ const closedIssues = async () => {
 
 }
 
+
 const displayIssues = (cards) => {
 
     cards.forEach(issue => {
         const card = document.createElement('div');
-        issue.status == "open" ? card.className = "shadow-md rounded-lg border-t-3 border-t-green-500" :
-            card.className = "shadow-md rounded-lg border-t-3 border-t-purple-500";
+        card.onclick = () => showIssueModal(issue.id);
+
+        issue.status == "open" ? card.className = "shadow-md rounded-lg border-t-3 border-t-green-500" : card.className = "shadow-md rounded-lg border-t-3 border-t-purple-500";
+
         card.innerHTML = `
     <div class="p-4 space-y-4">
                         <!-- Priority -->
@@ -107,10 +167,14 @@ const displayIssues = (cards) => {
                         <!-- labels -->
                         <div class="pb-5 mt-4 ">
 
-                            <span class="bg-error/10 text-red-500 border border-red-200 h-[18px] text-[10px] font-medium py-[2px] px-[2px] text-center rounded-3xl">${issue.labels[0].toUpperCase()}</span>
+                            ${{bug : `<span class="bg-error/10 text-red-500 border border-red-200 h-[18px] text-[10px] font-medium py-[2px] px-[4px] text-center rounded-3xl">${issue.labels[0].toUpperCase()}</span>`,
+                            enhancement:`<span class="bg-[#DEFCE8] text-[#00A96E] border border-[#BBF7D0] h-[18px] text-[10px] font-medium py-[2px] px-[4px] text-center rounded-3xl">${issue.labels[0].toUpperCase()}</span>`,
+                            documentation:`<span class="bg-blue-200 text-blue-600 border border-blue-300 h-[18px] text-[10px] font-medium py-[2px] px-[4px] text-center rounded-3xl">${issue.labels[0].toUpperCase()}</span>`}[issue.labels[0]]
+
+                            }
 
                             ${issue.labels[1] != undefined ? `
-                                <span class="bg-warning/10 text-yellow-600 border border-yellow-300 h-[18px] text-[10px] font-medium py-[2px] px-[2px] text-center rounded-3xl">
+                                <span class="bg-warning/10 text-yellow-600 border border-yellow-300 h-[18px] text-[10px] font-medium py-[2px] px-[4px] text-center rounded-3xl">
                                 ${issue.labels[1].toUpperCase()}
                                 </span>`: ""}
                            
@@ -122,7 +186,7 @@ const displayIssues = (cards) => {
                     <!-- Author & Created At -->
                     <div class="text-[#64748B] text-[12px] space-y-2 border-t-2 border-t-gray-200 p-4">
                         <h1> <span>#${issue.id}</span>  ${issue.author}</h1>
-                        <h1>${issue.createdAt}</h1>
+                        <h1>${new Date(issue.createdAt).toLocaleDateString("en-GB")}</h1>
                     </div>
     
     `
@@ -146,6 +210,7 @@ const displayIssues = (cards) => {
 
 
     });
+
 
 }
 
